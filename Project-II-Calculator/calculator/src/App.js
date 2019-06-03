772f30754381229a873d284ed63d16272b153002
Dannyyzz <dannyzz@live.com>
2019-06-02 20:08:08 -0400

    Buttons are not showing on app.js

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import ButtonsContainer from './components/ButtonComponents/ButtonsContainer.js';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js';
+
 
 const App = () => {
   return (
-    
+    <div className="calculator">
+    <CalculatorDisplay result="Example" />
+    <ButtonsContainer />
+    </div>
   );
 };
 

42e6e108a5ead442c6b7e92e0b0fe7b5e4b2582c
Dannyyzz <dannyzz@live.com>
2019-06-02 20:16:50 -0400

    Refactored the calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonsContainer.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ButtonsContainer.js
@@ -5,14 +5,23 @@ import ActionButton from './ActionButton.js';
  function ButtonsContainer() {
   return (
     <div className="buttons-container">
+
       <div className="buttons-grid-left">
         <ActionButton buttonStyle="btn-wide" values={['clear']}  />
-        <NumberButton buttonStyle="number-button-grid" numbersArray={[3,2,1,6,5,4,9,8,7]}/>
+
+        <div className="number-buttons-container">
+
+        <NumberButton buttonStyle="number-button" numbersArray={[7,8,9,4,5,6,1,2,3]}/>
+
+        </div>
+
         <ActionButton buttonStyle="btn-wide" values={[0]} />
       </div>
+
       <div className="buttons-grid-right">
         <ActionButton buttonStyle="btn-wide" values={['÷','⨉', '-', '+', '=']} />
       </div>
+
     </div>
   );
 }

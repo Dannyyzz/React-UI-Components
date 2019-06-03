93f5c7125f4943f26f2ac958fb08493bc948ef05
Dannyyzz <dannyzz@live.com>
2019-06-02 20:21:48 -0400

    Small edit

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Button.css';
 
 function ActionButton({ buttonStyle, values }) {
-    return values.map(value => <button key={value} className={buttonStyle}>{value}</button>);
-  }
-  
+  return values.map(value => (
+    <button key={value} className={buttonStyle}>
+      {value}
+    </button>
+  ));
+}
+
 export default ActionButton;
